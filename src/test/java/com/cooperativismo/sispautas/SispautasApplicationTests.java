2e7b712ef520e4dbfe775c345711e8ab9da2cdc6
Daniel Queiroz <34477023+queirozdelfino@users.noreply.github.com>
2022-05-21 18:29:12 -0300

    Add files via upload

---
+++ src/test/java/com/cooperativismo/sispautas/SispautasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativismo.sispautas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SispautasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
